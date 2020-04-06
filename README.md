b610849eabd692fb582c639483244c4beaa5b353
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-04-06 12:34:15 -0500

    added the shop and the bars

---
+++ README.md
@@ -0,0 +1,14 @@
+# Nuclear Incremental
+## What is this?
+This game is a spin-off of Reactor Incremental. It has more features too.
+
+## How do I play?
+Go to Kongregate or it's website, [here](https://daverainbowin.github.io/nuclearIncremental)
+
+### Starting
+You can start by picking up coins to get 10$.
+
+## Credits
+Main Developer: Ice
+???: SilverHydra6078
+3D Artist: PowerOne
--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+var currentHeat = 0;
+var currentCash = 0;
+var currentPower = 0;
+var maxHeat = 1000;
+var maxPower = 1000;
+var pctPower;
+var pctHeat;
+setInterval(update, 10);
+function powerButton() {
+  if (currentCash < 10) {
+    currentCash++;
+    if (currentCash >= 10) {
+      get("powerBtn").innerHTML = "Sell all power";
+    } else {
+      get("powerBtn").innerHTML = "Pick up some coins";
+    }
+  } else {
+    currentCash += currentPower;
+    currentPower = 0;
+  }
+}
+function update() {
+  pctPower = (currentPower/maxPower) * 100;
+  pctHeat = (currentHeat/maxHeat) * 100;
+
+  get("power").style.width = pctPower + "%";
+  get("heat").style.width = pctHeat + "%";
+  get("money").innerHTML = currentCash + "$"; 
+}
 function openTab(pageName) {
   var i, tabcontent;
   tabcontent = document.getElementsByClassName("tabShop");
@@ -5,4 +34,7 @@ function openTab(pageName) {
     tabcontent[i].style.display = "none";
   }
   document.getElementById(pageName).style.display = "block";
+}
+function get(id) {
+  return document.getElementById(id);
 }
