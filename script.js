98b2fec294d93baf9d1eb5b08d286b7b565ced2d
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-04-06 19:18:02 -0500

    add hydrogen img

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var currentHeat = 0;
 var currentCash = 0;
+var currentRad = 0;
 var currentPower = 0;
 var maxHeat = 1000;
 var maxPower = 1000;
+var maxRad = 100;
+var powerSec = 0;
+var heatSec = 0;
+var radSec = 0;
+var mapCells = 256;
 var pctPower;
 var pctHeat;
+var pctRad;
+var selectCell = "";
 setInterval(update, 10);
+setInterval(updateSec, 1000);
 function powerButton() {
   if (currentCash < 10) {
     currentCash++;
@@ -19,13 +28,28 @@ function powerButton() {
     currentPower = 0;
   }
 }
+function heatButton() {
+  if (currentHeat > 0) {
+    currentHeat--;
+    if (currentHeat < 0) {
+      currentHeat = 0;
+    }
+  }
+}
 function update() {
   pctPower = (currentPower/maxPower) * 100;
   pctHeat = (currentHeat/maxHeat) * 100;
-
+  pctRad = (currentRad/maxRad) * 100;
   get("power").style.width = pctPower + "%";
+  get("power").innerHTML = currentPower + "/" + maxPower;
   get("heat").style.width = pctHeat + "%";
+  get("heat").innerHTML = currentHeat + "/" + maxHeat;
   get("money").innerHTML = currentCash + "$"; 
+}
+function updateSec() {
+  currentPower += powerSec;
+  currentHeat += heatSec;
+  currentRad += radSec;
 }
 function openTab(pageName) {
   var i, tabcontent;
